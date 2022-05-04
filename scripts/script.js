271a83090b771cdd36c87c049d05e3b3c5fb22fd
Fauzan <fauzanffl031202@gmail.com>
2022-05-04 22:59:11 +0700

    mengubah toggle menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,10 @@
-
 function menu_active(){
-    const toggle_menu = document.querySelector(".toggle-menu");
-    
+    const toggle = document.querySelector(".toggle");
     const check = document.getElementById("check");
     
     if(check.checked == true){
-        toggle_menu.classList.add("menu-active");
+        toggle.classList.add("menu-active");
     } else {
-        toggle_menu.classList.remove("menu-active");
+        toggle.classList.remove("menu-active");
     }
 }
